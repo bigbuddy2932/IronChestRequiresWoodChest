f477afd4eda41f33a7976c4a2f8df738baa71c01
Benjamin Roeder <renboeder@gmail.com>
2020-11-26 13:22:20 -0600

    Initial release

---
+++ iron_chest_requires_wood_chest_1.0.0/prototypes/recipes.lua
@@ -0,0 +1,13 @@
+data:extend({
+  {
+    type = "recipe",
+    name = "iron-chest",
+    ingredients =
+    {
+      {"wood-chest", 1},
+      {"iron-plate", 4}
+    },
+    result = "iron-chest",
+    result_count = 1
+  }
+})
