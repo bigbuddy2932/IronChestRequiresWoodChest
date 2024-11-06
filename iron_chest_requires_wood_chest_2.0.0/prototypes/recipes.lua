5b79e9dfb57671a79bc8bc3b83a831ada28ec4da
Sonata Roeder <sonata.roeder@proton.me>
2024-11-06 04:36:40 -0600

    2.0 update

---
+++ iron_chest_requires_wood_chest_2.0.0/prototypes/recipes.lua
@@ -0,0 +1,18 @@
+data:extend({
+  {
+    type = "recipe",
+    name = "iron-chest",
+    ingredients =
+    {
+      {name = "wooden-chest", amount = 1, type = "item"},
+      {name = "iron-plate", amount = 4, type = "item"}
+    },
+    results = {
+      {
+        amount = 1,
+        name = "iron-chest",
+        type = "item"
+      }
+    }
+  }
+})
